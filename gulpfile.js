36d2bb4961fc125aae28a160328e591ce89ef57f
tong-fatt <tho662003@yahoo.com>
2017-02-05 01:37:25 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	minifyCSS = require('gulp-minify-css');
+
+gulp.task('scripts', function(){
+	gulp.src('js/**/*.js')
+		.pipe(uglify())
+		.pipe(rename('app.min.js'))
+		.pipe(gulp.dest('js/'));
+
+});
+
+	gulp.task('styles', function(){
+		gulp.src('css/*.css')
+			.pipe(minifyCSS())
+			.pipe(gulp.dest('minCSS'))
+	});
+
+
+	gulp.task('watch', function(){
+		gulp.watch('js/*.js',['scripts']);
+		gulp.watch('css/*.css',['styles']);	
+	});
+	
+	gulp.task('default',['scripts', 'styles', 'watch']);
